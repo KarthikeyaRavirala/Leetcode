313f8bb0d97b5e2867b35b6fd75e6d05a063122f
karthikeya ravirala <132122712+KarthikeyaRavirala@users.noreply.github.com>
2024-02-17 12:16:45 +0530

    Update Valid parentheses.py

--- DAY-02/20. Valid Parentheses/Valid parentheses.py
+++ DAY-02/20. Valid Parentheses/Valid parentheses.py
@@ -1,1 +1,18 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        a=[]
+        b= {')': '(', '}': '{', ']': '['}
 
+        for char in s:
+            if char in b:
+                if not a or a.pop() != b[char]:
+                    return False
+            else:
+                a.append(char)
+        return not a
+
+def main():
+    c=sol()
+    inp=input()
+    result=c.isValid(inp)
+    print(result)
